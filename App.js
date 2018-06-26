598a22153824f4f96529ef14106f87806ec03dd9
masmi <mohamed.asmi.prestataire@sfr.fr>
2018-06-26 23:57:42 +0200

    second commit

--- App.js
+++ App.js
@@ -1,91 +1,125 @@
-import React from 'react';
-import { Button, StyleSheet, StatusBar, Text, View, ListView, Image } from 'react-native';
+import React from "react";
+import {
+  Button,
+  StyleSheet,
+  StatusBar,
+  TouchableOpacity,
+  Text,
+  View,
+  ListView,
+  TextInput
+} from "react-native";
 import service from "./helpers/service";
 
 export default class App extends React.Component {
-
   constructor(props) {
     super(props);
-    this.state = { 
-      somme: 0, 
-      isLoading: true 
+    this.state = {
+      isLoading: true,
+      allCountries: [],
+      countries: []
     };
   }
 
+  reset = () => {
+    //    this.setState({ somme: 0})
+  };
+
+  handleCountrySearchChange = value => {
+    this.setState({
+      countries: this.state.allCountries.filter(
+        country =>
+          country.name.toUpperCase().indexOf(value.toUpperCase()) !== -1
+      )
+    });
+  };
+
   componentWillMount() {
     var self = this;
-    return service.getAllCountries()
-                .then(function(response){
-                  self.setState({isLoading: false, countries: response.data})
-                });
-  }
-
-  sum = (e) => {
-    e.preventDefault(); 
-    this.setState({ sommxe: this.state.somme + 1})
-  }
-
-  substract = (e) => {
-    e.preventDefault(); 
-    this.setState({ somme: this.state.somme - 1})
-  }
-
-  reset = (e) => {
-    e.preventDefault(); 
-    this.setState({ somme: 0})
+    return service.getAllCountries().then(function(response) {
+      self.setState({
+        isLoading: false,
+        allCountries: response.data,
+        countries: response.data
+      });
+    });
   }
 
   render() {
-    if(this.state.isLoading) {
-      return <Text >Loading...</Text>
+    if (this.state.isLoading) {
+      return <Text>Loading...</Text>;
     }
-    const dataSource = new ListView.DataSource({rowHasChanged:(r1,r2) => r1 != r2});
+
+    const dataSource = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 != r2
+    });
     return (
       <View style={styles.container}>
         <StatusBar hidden={true} />
-        <View style={styles.bloc2}>
-          <View style={styles.button}><Button title="Add" onPress={this.sum}/></View>
-          <View style={styles.button}><Button title="Sub" onPress={this.substract}/></View>
-          <View style={styles.button}><Button  title="Reset" onPress={this.reset}/></View>
-        </View>
-        <ListView
-          dataSource={dataSource.cloneWithRows(this.state.countries)}
-          renderRow={(rowData) =>
-            <View>
-               <Text>{rowData.name}</Text>
-          </View>} 
+        <View style={styles.container}>
+          <TextInput
+            style={styles.input}
+            underlineColorAndroid="transparent"
+            placeholder="Search country"
+            placeholderTextColor="#9a73ef"
+            autoCapitalize="none"
+            onChangeText={this.handleCountrySearchChange}
           />
-        <View>
-        <Text style={styles.bloc3}>{this.state .somme}</Text>
+
+          <TouchableOpacity style={styles.submitButton} onPress={this.reset()}>
+            <Text style={styles.submitButtonText}> Reset </Text>
+          </TouchableOpacity>
+
+          <View>
+          <Text style={styles.result}>{this.state.countries.length} countries</Text>
+          <Text style={styles.result}>Result : </Text>
+          </View>
+          <ListView
+            dataSource={dataSource.cloneWithRows(this.state.countries)}
+            renderRow={rowData => (
+              <View>
+                <Text>{rowData.name}</Text>
+              </View>
+            )}
+          />
         </View>
       </View>
     );
   }
 }
 
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#FFF',
-    alignItems: 'center',
-    justifyContent: 'center',
-    margin: 20,
+    paddingTop: 23
   },
   bloc1: {
-    paddingBottom: 15,
+    paddingBottom: 15
   },
   bloc2: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: "row"
   },
-  bloc3: {
-    fontSize: 250,
+  result: {
+    fontSize: 50
   },
   button: {
     paddingLeft: 15,
     width: 100,
-    height: 50,
+    height: 50
+  },
+  input: {
+    margin: 15,
+    height: 40,
+    borderColor: "#7a42f4",
+    borderWidth: 1
+  },
+  submitButton: {
+    backgroundColor: "#7a42f4",
+    padding: 10,
+    margin: 15,
+    height: 40
+  },
+  submitButtonText: {
+    color: "white"
   }
 });
